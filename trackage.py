c8447190ab59f03a6f53b2edeff0b8b9f3e39e99
Brianne Bender <s529231@nwmissouri.edu>
2020-07-28 18:27:02 -0500

    get argparser to accept optional arguments

--- trackage.py
+++ trackage.py
@@ -5,20 +5,20 @@ from functions import add_package, del_package, track_all, track_one
 # Create the parser
 parser = argparse.ArgumentParser(prog='trackage', description='Track your packages')
 
-FUNCTION_MAP = {'add_package' : add_package,
-                'delete_package' : del_package,
+FUNCTION_MAP = {'add' : add_package,
+                'delete' : del_package,
                 'track_all' : track_all,
                 'track_one' : track_one}
 
 # Add the arguments
 parser.add_argument('command', choices=FUNCTION_MAP.keys(), 
-                        help='Enter one of the following commands: /nadd_package (arguments: tracking_number, shipping_service) /ndelete_package (arguments: tracking_number)/ntrack_all (arguments: none)/ntrack_one (arguments: tracking_number shipping_service')
+                        help='Enter one of the following commands: add (arguments: tracking_number, shipping_service), delete (arguments: tracking_number), track_all (arguments: none), track_one (arguments: tracking_number shipping_service')
 # optional arguments depending on command
-parser.add_argument('tracking_number',
+parser.add_argument('-tn',
                        metavar='tracking_number',
                        type=int,
                        help='The tracking number of package to be tracked')
-parser.add_argument('service',
+parser.add_argument('-s',
                         metavar='shipping_service',
                         type=str,
                         help='The service shipping the package (usps, ups, fedex)')
@@ -29,11 +29,11 @@ args = parser.parse_args()
 # Execute correct function based on input command
 func = FUNCTION_MAP[args.command]
 if(func == add_package):
-    func(args.tracking_number, args.shipping_service)
+    func(args.tn, args.s)
 elif(func == del_package):
-    func(args.tracking_number)
+    func(args.tn)
 elif(func == track_all):
     func()
 elif(func == track_one):
-    func(args.tracking_number, args.service)
+    func(args.tn, args.s)
 
